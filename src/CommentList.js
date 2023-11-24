359b5ef9d12af030d95eaba393aac3f4b0fe1d4a
warisamir <warisamir1918@gmail.com>
2023-11-24 11:06:27 +0530

    only this

--- src/CommentList.js
+++ src/CommentList.js
@@ -3,8 +3,19 @@ import React from 'react'
 export default ({comments})=> {
  
 
-    const renderedComents=comments.map(comments=>{
-        return <li key={comments.id}>{comments.content}</li>;
+    const renderedComents=comments.map(comment=>{
+    
+      let content;
+      if(comment.status==='approved'){
+      content=comment.content;
+      }
+      if(comment.status==='pending'){
+        content='this comment is awaitng';
+      }
+      if(comment.status==='rejected'){
+        content='this comment is rejected';
+      }
+        return <li key={comment.id}>{content}</li>;
     });
   return (
     <ul>
